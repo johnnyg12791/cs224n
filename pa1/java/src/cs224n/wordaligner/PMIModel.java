1de18daf47c1ed2eb1cb09a87e6dcf2e5d6c8447
John Barrett Gold <johngold@corn15.stanford.edu>
2014-10-01 19:55:19 -0700

    adding 3 classes, pmi and 2 ibms

---
+++ pa1/java/src/cs224n/wordaligner/PMIModel.java
@@ -0,0 +1,20 @@
+package cs224n.wordaligner;
+import java.util.List;
+
+
+
+public class PMIModel implements WordAligner {
+
+	@Override
+	public Alignment align(SentencePair sentencePair) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void train(List<SentencePair> trainingData) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
