a32ca54dab5c0dcaf11db47d95b4a13666ab49be
John Barrett Gold <johngold@corn23.stanford.edu>
2014-10-21 00:02:38 -0700

    more

--- pa2/java/src/cs224n/assignment/PCFGParser.java
+++ pa2/java/src/cs224n/assignment/PCFGParser.java
@@ -7,6 +7,7 @@ import cs224n.util.Triplet;
 
 import java.util.*;
 import cs224n.assignment.*;
+import cs224n.assignment.Grammar.UnaryRule;
 
 /**
  * The CKY PCFG Parser you will implement.
@@ -14,6 +15,8 @@ import cs224n.assignment.*;
 public class PCFGParser implements Parser {
     private Grammar grammar;
     private Lexicon lexicon;
+	private Map<Triplet<Integer, Integer, String>, Pair<Map<String, Integer[]>, Map<String, Integer[]> >> back;
+
 
     public void train(List<Tree<String>> trainTrees) {
         // TODO: before you generate your grammar, the training trees
@@ -39,19 +42,41 @@ public class PCFGParser implements Parser {
     			score.get(i).add(new Counter<String>());
     		}
     	}
-    	Map<Triplet<Integer, Integer, String>, Pair<Map<String, Integer[]>, Map<String, Integer[]> >> back;
     	back = new HashMap<Triplet<Integer, Integer, String>, Pair<Map<String, Integer[]>, Map<String, Integer[]> >>();
     	for (int i = 0; i < sentence.size(); i++) {
     		for (String nonterm : lexicon.getAllTags()) {
     			double d = lexicon.scoreTagging(sentence.get(i), nonterm);
-    			score.get(i).get(i + 1).setCount(nonterm, d);
+    			score.get(i).get(i + 1).setCount(nonterm, d);//WHY do we need to split this up??
     		}
     		boolean added = true;
     		while (added) {
     			added = false;
+    			for (String A : lexicon.getAllTags()){//NP a nonterm??
+    				List<UnaryRule> rules = grammar.getUnaryRulesByChild(A);
+    				for (UnaryRule rule: rules){
+    					String B = rule.parent;
+    					double probability = rule.score*score.get(i).get(i+1).getCount(B);//It's okay if = 0
+    					if(probability > score.get(i).get(i+1).getCount(A)){
+    						score.get(i).get(i+1).setCount(A, probability);
+    						addToBackMap(i, A, B);
+    						added = true;
+    					}
+    				}
+    			}
     		}
     	}
     	
         return null;
+    }
+    
+    //Create the new datastructures necessary for our backtrace map, then add it
+    private void addToBackMap(int i, String A, String B){
+    	Triplet<Integer, Integer, String> triple = new Triplet<Integer, Integer, String>(i, i+1, A);
+    	Map<String, Integer[]> AMap = new HashMap<String, Integer[]>();
+    	AMap.put(A, new Integer[]{i, i+1});
+    	Pair<Map<String, Integer[]>, Map<String, Integer[]>> pair = new Pair<Map<String, Integer[]>, Map<String, Integer[]>>(AMap, null);
+
+    	back.put(triple, pair);
+    	
     }
 }
