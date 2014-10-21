9c2970893788964083315e9092be7fa502a02e82
Justin Matthew Salloum <jsalloum@corn16.stanford.edu>
2014-10-20 23:17:57 -0700

    started implementing the algorithm

--- pa2/java/src/cs224n/assignment/PCFGParser.java
+++ pa2/java/src/cs224n/assignment/PCFGParser.java
@@ -1,7 +1,12 @@
 package cs224n.assignment;
 
-import cs224n.ling.Tree;
+import cs224n.ling.*;
+import cs224n.util.Counter;
+import cs224n.util.Pair;
+import cs224n.util.Triplet;
+
 import java.util.*;
+import cs224n.assignment.*;
 
 /**
  * The CKY PCFG Parser you will implement.
@@ -13,12 +18,40 @@ public class PCFGParser implements Parser {
     public void train(List<Tree<String>> trainTrees) {
         // TODO: before you generate your grammar, the training trees
         // need to be binarized so that rules are at most binary
+    	for (int i = 0; i < trainTrees.size(); i++) {
+    		//Trees.PennTreeRenderer.render(trainTrees.get(i));
+    		trainTrees.set(i, TreeAnnotations.annotateTree(trainTrees.get(i)));
+    		//Trees.PennTreeRenderer.render(trainTrees.get(i));
+    	}
         lexicon = new Lexicon(trainTrees);
         grammar = new Grammar(trainTrees);
     }
 
     public Tree<String> getBestParse(List<String> sentence) {
         // TODO: implement this method
+    	System.out.println(sentence);
+    	
+    	// Initialization
+    	ArrayList<ArrayList<Counter<String>>> score = new ArrayList<ArrayList<Counter<String>>>();
+    	for (int i = 0; i < sentence.size() + 1; i ++) {
+    		score.add(new ArrayList<Counter<String>> ());
+    		for (int j = 0; j < sentence.size() + 1; j++) {
+    			score.get(i).add(new Counter<String>());
+    		}
+    	}
+    	Map<Triplet<Integer, Integer, String>, Pair<Map<String, Integer[]>, Map<String, Integer[]> >> back;
+    	back = new HashMap<Triplet<Integer, Integer, String>, Pair<Map<String, Integer[]>, Map<String, Integer[]> >>();
+    	for (int i = 0; i < sentence.size(); i++) {
+    		for (String nonterm : lexicon.getAllTags()) {
+    			double d = lexicon.scoreTagging(sentence.get(i), nonterm);
+    			score.get(i).get(i + 1).setCount(nonterm, d);
+    		}
+    		boolean added = true;
+    		while (added) {
+    			added = false;
+    		}
+    	}
+    	
         return null;
     }
 }
