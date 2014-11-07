d06bfe8f5ca1c011c0cdfc95bff1041b9c004f3f
Justin Matthew Salloum <jsalloum@corn23.stanford.edu>
2014-11-07 10:57:15 -0800

    Better baseline working with head tagging

--- pa3/java/src/cs224n/corefsystems/BetterBaseline.java
+++ pa3/java/src/cs224n/corefsystems/BetterBaseline.java
@@ -15,15 +15,16 @@ import cs224n.util.Pair;
 
 public class BetterBaseline implements CoreferenceSystem {
 
-	CounterMap<Pair<Mention, Mention>, Entity> coreferentPairs;
-	CounterMap<List<String>, Entity> mentionEntityMap;
-	Map<List<String>, Set<Entity>> entityListMap;
+	Map<String, Entity> clusterMap;
+	Map<Mention, Entity> usedMap;
+	CounterMap<String, String> pairMap;
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		coreferentPairs = new CounterMap<Pair<Mention, Mention>, Entity>();
-		mentionEntityMap = new CounterMap<List<String>, Entity>();
-		entityListMap = new HashMap<List<String>, Set<Entity>>();
+		clusterMap = new HashMap<String, Entity>();
+		usedMap = new HashMap<Mention, Entity>();
+		
+		pairMap = new CounterMap<String, String>();
 		for(Pair<Document, List<Entity>> pair : trainingData){
 			//--Get Variables
 			Document doc = pair.getFirst();
@@ -34,102 +35,52 @@ public class BetterBaseline implements CoreferenceSystem {
 			//--Iterate over mentions
 			for(Mention m : mentions){
 				//System.out.println(m);
+				
 			}
+			
+			for (Entity e : clusters) {
+//				for (Mention m : e.mentions) {
+//					clusterMap.put(m.gloss(), e);
+//				}
+			}
+			
 			//--Iterate Over Coreferent Mention Pairs
 			for(Entity e : clusters){
 				for(Pair<Mention, Mention> mentionPair : e.orderedMentionPairs()){
-					coreferentPairs.incrementCount(mentionPair, e, 1);
-					mentionEntityMap.incrementCount(mentionPair.getFirst().text(), e, 1);
-					mentionEntityMap.incrementCount(mentionPair.getSecond().text(), e, 1);
-					if (entityListMap.containsKey(mentionPair.getFirst())) {
-						Set<Entity> entities = entityListMap.get(mentionPair.getFirst());
-						entities.add(e);
-						//System.out.println(entities);
-						entityListMap.put(mentionPair.getFirst().text(), entities);
-						//System.out.println("EXISTING");
-					} else {
-						Set<Entity> entities = new HashSet<Entity>();
-						entities.add(e);
-						entityListMap.put(mentionPair.getFirst().text(), entities);
-						//System.out.println("NEW");
-					}
-
-					if (entityListMap.containsKey(mentionPair.getSecond())) {
-						Set<Entity> entities = entityListMap.get(mentionPair.getSecond());
-						entities.add(e);
-						entityListMap.put(mentionPair.getSecond().text(), entities);
-					} else {
-						Set<Entity> entities = new HashSet<Entity>();
-						entities.add(e);
-						entityListMap.put(mentionPair.getSecond().text(), entities);
-					}
-
+					//pairMap.incrementCount(mentionPair.getFirst().headWord(), mentionPair.getSecond().headWord(), 1.0);
 					//System.out.println(""+mentionPair.getFirst() + " and " + mentionPair.getSecond() + " are coreferent");
 				}
 			}
-			//System.out.println(mentionEntityMap);
-			//break;
 		}
+		//System.out.println(clusterMap);
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-
+		//(variables)
 		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
 		Map<String,Entity> clusters = new HashMap<String,Entity>();
-		Set<Entity> usedEntities = new HashSet<Entity>();
-
-		for (Mention m : doc.getMentions()) {
-			//			if (m.gloss().equals("God the Protector")) {
-			//				System.out.println(m.sentence.parse);
-			//				System.out.println(m.parse);
-			//				System.out.println(m.beginIndexInclusive + " "+m.endIndexExclusive);
-			//				System.out.println(m.gloss());
-			//			}
-
-			Set<Entity> entities = entityListMap.get(m.text());
-			int max = Integer.MIN_VALUE;
-			Entity maxEntity = null;
-			//System.out.println(entities);
-			String mentionString = m.gloss();
-			
-			if (clusters.containsKey(mentionString)) {
-				mentions.add(m.markCoreferent(clusters.get(mentionString)));
-				//System.out.println("FOUND");
+		
+		Map<String, Entity> headMap = new HashMap<String, Entity>();
+		//(for each mention...)
+		int i = 0;
+		for(Mention m : doc.getMentions()){
+			//(...get its text)
+			//String mentionString = m.gloss();
+			String headWord = m.headWord();
+			//(...if we've seen this text before...)
+			if (headMap.containsKey(headWord)) {
+				System.out.println("Head Word: " + headWord + ", Entity: " + headMap.get(headWord));
+				mentions.add(m.markCoreferent(headMap.get(headWord)));
 			} else {
-				ClusteredMention newCluster;
-				if (entities != null) {
-					for (Entity e : entities) {
-						if (mentionEntityMap.getCount(m.text(), e) > max) {
-							max = (int) mentionEntityMap.getCount(m.text(), e);
-							maxEntity = e;
-						}
-						//System.out.println(mentionEntityMap.getCount(m.text(), e));
-					}
-					if (maxEntity != null) {
-						if (usedEntities.contains(maxEntity)) {
-							newCluster = m.markSingleton();
-							System.out.println("Already used Entity!");
-						} else {
-							newCluster = m.markCoreferent(maxEntity);
-						}
-						usedEntities.add(maxEntity);
-						//mentions.add(m.markCoreferent(maxEntity));
-					} else { 
-						newCluster = m.markSingleton();
-						//mentions.add(m.markSingleton());
-					}
-				} else {
-					newCluster = m.markSingleton();
-					//mentions.add(m.markSingleton());
-				}
+				ClusteredMention newCluster = m.markSingleton();
 				mentions.add(newCluster);
-				clusters.put(mentionString, newCluster.entity);
+				headMap.put(headWord, newCluster.entity);
 			}
-			//break;
+
+			System.out.println(mentions);
 		}
-		//System.out.println(mentions);
+		//(return the mentions)
 		return mentions;
 	}
 
