0f4034f843321f254e2ffaa09c14cc54d784321d
Justin Matthew Salloum <jsalloum@corn23.stanford.edu>
2014-11-07 20:48:37 -0800

    More rulessss

--- pa3/java/src/cs224n/corefsystems/RuleBased.java
+++ pa3/java/src/cs224n/corefsystems/RuleBased.java
@@ -43,7 +43,9 @@ public class RuleBased implements CoreferenceSystem {
 		Map<Integer, Integer> ourMentionMap = new HashMap<Integer, Integer>(); 
 		Map<String, Integer> exactMatch = new HashMap<String, Integer>();
 		
-		//(for each mention...)
+		/*
+		 * First pass: exact string match
+		 */
 		for(int i = 0; i < doc.getMentions().size(); i++){
 			Mention m = doc.getMentions().get(i);
 			String mentionString = m.gloss();
@@ -55,6 +57,89 @@ public class RuleBased implements CoreferenceSystem {
 			}
 		}
 		
+		/*
+		 * Second pass: exact head word match
+		 */
+		Map<String, Integer> headMatch = new HashMap<String, Integer>();
+		for (int i = 0; i < doc.getMentions().size(); i++) {
+			Mention m = doc.getMentions().get(i);
+			String headWord = m.headWord();
+			if(headMatch.containsKey(headWord)) {
+				ourMentionMap.put(i, headMatch.get(headWord));
+			} else {
+				//ourMentionMap.put(i, -1);
+				headMatch.put(headWord, i);
+			}
+		}
+		
+		/*
+		 * Third pass: lemma word match
+		 */
+		Map<String, Integer> lemmaMatch = new HashMap<String, Integer>();
+		for (int i = 0; i < doc.getMentions().size(); i++) {
+			Mention m = doc.getMentions().get(i);
+			String lemmaWord = m.headToken().lemma();
+			if(lemmaMatch.containsKey(lemmaWord)) {
+				ourMentionMap.put(i, lemmaMatch.get(lemmaWord));
+				//System.out.println("matched lemma");
+			} else {
+				//ourMentionMap.put(i, -1);
+				lemmaMatch.put(lemmaWord, i);
+			}
+		}
+		
+		/*
+		 * Forth pass: headword substring
+		 */
+		Map<String, Integer> substringMatch = new HashMap<String, Integer>();
+		for (int i = 0; i < doc.getMentions().size(); i++) {
+			Mention m = doc.getMentions().get(i);
+			String headWord = m.headWord();
+			boolean addedToMap = false;
+			for (String str : substringMatch.keySet()) {
+				if (str.length() > 4 && headWord.length() > 4) {
+					if (str.contains(headWord)) {
+						ourMentionMap.put(i, substringMatch.get(str));
+						addedToMap = true;
+					}
+					else if (headWord.contains(str)) {
+						ourMentionMap.put(i, substringMatch.get(str));
+						addedToMap = true;
+					}
+				}
+			}
+			if (!addedToMap) {
+				substringMatch.put(headWord, i);
+			}
+		}
+		
+		/*
+		 * Fifth pass: I, me, my, myself, mine etc
+		 */
+		Set<String> meMySet = new HashSet<String>(Arrays.asList("i", "me", "my", "myself", "mine"));
+		checkSimilarThings(meMySet, "i", doc, ourMentionMap);
+		
+		/*
+		 * Sixth pass: us, we, our, ours, ourselves
+		 */
+		Set<String> usWeSet = new HashSet<String>(Arrays.asList("we", "our", "ours", "ourselves"));
+		checkSimilarThings(usWeSet, "we", doc, ourMentionMap);
+		
+		
+//		Map<String, Integer> meMyMatch = new HashMap<String, Integer>();
+//		for (int i = 0; i < doc.getMentions().size(); i++) {
+//			Mention m = doc.getMentions().get(i);
+//			String headWord = m.headToken().lemma().toLowerCase();
+//			if (meMySet.contains(headWord)) {
+//				System.out.println("We found a meMy word");
+//				if (meMyMatch.containsKey("i")) {
+//					ourMentionMap.put(i, meMyMatch.get("i"));
+//					System.out.println("MEMYMATCH");
+//				} else {
+//					meMyMatch.put("i", i);
+//				}
+//			}
+//		}
 		
 		//This is the merge, should come at the very end
 		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
@@ -71,8 +156,26 @@ public class RuleBased implements CoreferenceSystem {
 			}
 		}
 		
-		System.out.println(ourMentionMap);
-		System.out.println(exactMatch);
+		//System.out.println(ourMentionMap);
+		//System.out.println(exactMatch);
+		//System.out.println(substringMatch);
 		return mentions;
+	}
+	
+	private void checkSimilarThings(Set<String> set, String keyWord, Document doc, Map<Integer, Integer> ourMentionMap) {
+		Map<String, Integer> matches = new HashMap<String, Integer>();
+		for (int i = 0; i < doc.getMentions().size(); i++) {
+			Mention m = doc.getMentions().get(i);
+			String headWord = m.headToken().lemma().toLowerCase();
+			if (set.contains(headWord)) {
+				//System.out.println("We found a meMy word");
+				if (matches.containsKey(keyWord)) {
+					ourMentionMap.put(i, matches.get(keyWord));
+					//System.out.println("MATCH");
+				} else {
+					matches.put(keyWord, i);
+				}
+			}
+		}
 	}
 }
